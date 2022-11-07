c2aa5575082152683c4aaadd0e5e91504020b903
AlexChapman18 <alex.d.chapman@outlook.com>
2022-11-07 16:14:40 +0000

    stuff n ting

--- src/main.java
+++ src/main.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) throws SQLException {
-
         System.out.println("Connecting to database...");
         Connection con = Utils.connect();
         System.out.println("Connected!");

c83ede5e70f24f40f816c41a37eae40231f1160f
AlexChapman18 <alex.d.chapman@outlook.com>
2022-11-07 14:07:49 +0000

    successfully connected to the database : )

--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.*;
 
@@ -12,10 +14,10 @@ public class Utils {
             System.out.println(list.nextElement());
     }
 
-    public Connection connect() throws SQLException {
+    public static Connection connect(String username, String password) throws SQLException {
         Connection con = null; // a Connection object
         try {
-            con = DriverManager.getConnection("jdbc:mysql://server/dbname", "userID", "password");
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/"+username, username, password);
         }
         catch (Exception ex) {
             ex.printStackTrace();
@@ -25,8 +27,26 @@ public class Utils {
         return con;
     }
 
-    public void closeConnection(Connection con) throws SQLException {
+    public static void closeConnection(Connection con) throws SQLException {
         if (con != null)
             con.close();
+    }
+
+    public static String[] getCredentials(){
+        String[] creds = new String[2];
+
+        try {
+            File myObj = new File("Credentials.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                creds[0] = myReader.nextLine().split("=")[1];
+                creds[1] = myReader.nextLine().split("=")[1];
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return creds;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
+import java.sql.*;
+import java.util.Arrays;
+
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws SQLException {
+        String[] creds = Utils.getCredentials();
+        Connection con = Utils.connect(creds[0], creds[1]);
+        System.out.println(con.getSchema());
+        Utils.closeConnection(con);
     }
 }

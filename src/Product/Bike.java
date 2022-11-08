70f1b716e28767833321c3841a4bc27a222cd2e3
AlexChapman18 <alex.d.chapman@outlook.com>
2022-11-08 17:17:43 +0000

    Started writing the Products classes

--- src/Product/Bike.java
+++ src/Product/Bike.java
@@ -1,4 +1,32 @@
 package Product;
 
-public class Bike {
+public class Bike extends Product{
+
+//    Declaring Variables
+    protected final int ASSEMBLYCOST = 10;
+    protected Wheels wheels;
+    protected Frame frame;
+    protected HandleBar handleBar;
+
+//    Constructor
+//    itemCost is calculated automatically
+    public Bike(Wheels wheels, Frame frame, HandleBar handleBar, String name, int serialNumber, String brandName) {
+        this.wheels = wheels;
+        this.frame = frame;
+        this.handleBar = handleBar;
+        float bikePrice = wheels.getItemCost() + frame.getItemCost() + handleBar.getItemCost() + ASSEMBLYCOST;
+        this.setProductDetails(name, serialNumber, bikePrice, brandName);
+    }
+
+//    Getters
+    public Wheels getWheels() {
+        return wheels;
+    }
+    public Frame getFrame() {
+        return frame;
+    }
+    public HandleBar getHandleBar() {
+        return handleBar;
+    }
+
 }
--- src/Product/Product.java
+++ src/Product/Product.java
@@ -1,4 +1,32 @@
 package Product;
 
-public class Product {
+public abstract class Product {
+
+//    Declaring Variables
+    protected String name;
+    protected int serialNumber;
+    protected float itemCost;
+    protected String brandName;
+
+//    Sets Product variables
+    protected void setProductDetails(String name, int serialNumber, float itemCost, String brandName){
+        this.name = name;
+        this.serialNumber = serialNumber;
+        this.itemCost = itemCost;
+        this.brandName = brandName;
+    }
+
+//    Getters
+    public String getName() {
+        return name;
+    }
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    public float getItemCost() {
+        return itemCost;
+    }
+    public String getBrandName() {
+        return brandName;
+    }
 }

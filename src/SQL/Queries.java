238e8d321b9034cf5e3655fbd93a21a11542f043
AlexChapman18 <alex.d.chapman@outlook.com>
2022-11-11 16:08:45 +0000

    started crating DbConnection and such

---
+++ src/SQL/Queries.java
@@ -0,0 +1,16 @@
+package SQL;
+
+import Product.Wheels;
+
+public class Queries {
+
+    public Wheels[] getWheels(int diameter, Wheels.Style style, Wheels.BrakeType brakeType){
+
+
+        query=...
+        results = DbConnection.executeQuery(query);
+
+
+//        return ;
+    }
+}

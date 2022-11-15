4ad4a19bd3d521f5ef883a8873b549bfa08cbd7a
Mohammad Tahir Khan <tahir026khan@gmail.com>
2022-11-15 22:32:21 +0000

    changes in actors

--- src/Order/Order.java
+++ src/Order/Order.java
@@ -1,4 +1,47 @@
 package Order;
 
 public class Order {
+    public int orderNumber;
+
+    public double totalCost;
+    public enum Status {
+        PENDING,
+        CONFIRMED,
+        FULFILLED
+    }
+    public Status status;
+
+    public Order (int orderNumber , double totalCost, Status status){
+        this.orderNumber=orderNumber;
+        this.totalCost=totalCost;
+        this.status=status;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
 }
+
+
+
+
--- src/Order/OrderDetails.java
+++ src/Order/OrderDetails.java
@@ -1,4 +1,40 @@
 package Order;
 
-public class OrderDetails {
+public class OrderDetails extends Order{
+    public String orderedItemName;
+    public int orderedItemQuantity;
+    public double itemCost;
+
+
+    public OrderDetails(int orderNumber, double totalCost, Status status, String orderedItemName, int orderedItemQuantity, double itemCost) {
+        super(orderNumber, totalCost, status);
+        this.orderedItemName=orderedItemName;
+        this.orderedItemQuantity=orderedItemQuantity;
+        this.itemCost=itemCost;
+    }
+
+    public String getOrderedItemName() {
+        return orderedItemName;
+    }
+
+    public void setOrderedItemName(String orderedItemName) {
+        this.orderedItemName = orderedItemName;
+    }
+
+    public int getOrderedItemQuantity() {
+        return orderedItemQuantity;
+    }
+
+    public void setOrderedItemQuantity(int orderedItemQuantity) {
+        this.orderedItemQuantity = orderedItemQuantity;
+    }
+
+    public double getItemCost() {
+        return itemCost;
+    }
+
+    public void setItemCost(double itemCost) {
+        this.itemCost = itemCost;
+    }
 }
+

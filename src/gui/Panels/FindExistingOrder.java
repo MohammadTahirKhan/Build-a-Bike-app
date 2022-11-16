5720f895199d47494267ce965f6daa244c21ec0b
Lumibar <47082617+lumibar@users.noreply.github.com>
2022-11-16 19:22:40 +0000

    Clean existing order ready for use with UI utils

--- src/gui/Panels/FindExistingOrder.java
+++ src/gui/Panels/FindExistingOrder.java
@@ -6,44 +6,27 @@ import java.awt.*;
 public class FindExistingOrder extends JPanel {
     public FindExistingOrder(){
         this.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-//        gbc.gridheight = 3;
-//        gbc.gridwidth = 3;
-        gbc.weightx = 0;
 
         JLabel reviewOrder = new JLabel("Review Order");
-        gbc.anchor = GridBagConstraints.CENTER;
-        reviewOrder.setFont(new Font("Arial", Font.BOLD, 20));
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.gridwidth = 3;
-        gbc.insets = new Insets(0, 5, 10, 5);
-        this.add(reviewOrder, gbc);
-        gbc.gridwidth = 1;
-        gbc.anchor = GridBagConstraints.LINE_START;
+        this.add(reviewOrder, createGbc(0,0));
 
         JTextField orderID = new JTextField("Order ID");
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.weightx = 0.7;
-        this.add(orderID, gbc);
-        gbc.weightx = 0;
+        this.add(orderID, createGbc(0,1));
 
         JButton findOrder = new JButton("Find Order");
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.gridwidth = 2;
-        this.add(findOrder, gbc);
-        gbc.gridwidth = 1;
+        this.add(findOrder, createGbc(0,2));
 
-        JButton backButton = new JButton("Back");
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        gbc.ipadx = 10;
-        gbc.insets = new Insets(0, 10, 5, 5);
-        this.add(backButton, gbc);
+//        JButton backButton = new JButton("Back");
+//        this.add(backButton, createGbc(0,3));
 
         this.setVisible(true);
+    }
+
+    private static GridBagConstraints createGbc(int x, int y) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.fill = GridBagConstraints.BOTH;
+        return gbc;
     }
 }

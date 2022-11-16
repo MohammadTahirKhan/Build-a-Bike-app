3bd43c906e598ccb6d5f3d65d70676b41538adf9
Lumibar <47082617+lumibar@users.noreply.github.com>
2022-11-16 19:36:42 +0000

    Staff Login page

---
+++ src/gui/Panels/StaffLogin.java
@@ -0,0 +1,43 @@
+package gui.Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StaffLogin extends JPanel {
+    private static final Insets WEST_INSETS = new Insets(5, 0, 5, 5);
+    private static final Insets EAST_INSETS = new Insets(5, 5, 5, 0);
+
+    public StaffLogin() {
+        this.setLayout(new GridBagLayout());
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+        JButton loginButton = new JButton("Login");
+
+        this.add(usernameLabel, createGbc(0, 0));
+        this.add(usernameField, createGbc(1, 0));
+        this.add(passwordLabel, createGbc(0, 1));
+        this.add(passwordField, createGbc(1, 1));
+        this.add(loginButton, createGbc(1, 2));
+    }
+
+    private static GridBagConstraints createGbc(int x, int y) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+        gbc.anchor = (x == 0) ? GridBagConstraints.WEST : GridBagConstraints.EAST;
+        gbc.fill = (x == 0) ? GridBagConstraints.BOTH
+                : GridBagConstraints.HORIZONTAL;
+
+        gbc.insets = (x == 0) ? WEST_INSETS : EAST_INSETS;
+        gbc.weightx = (x == 0) ? 0.1 : 1.0;
+        gbc.weighty = 1.0;
+        return gbc;
+    }
+}
+

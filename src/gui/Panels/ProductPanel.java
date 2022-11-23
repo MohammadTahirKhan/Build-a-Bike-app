1af911f32af4e3e9f8e54e5397a5f86527689a4a
Lumibar <47082617+lumibar@users.noreply.github.com>
2022-11-23 15:23:16 +0000

    Product panel class 2

--- src/gui/Panels/ProductPanel.java
+++ src/gui/Panels/ProductPanel.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public class ProductPanel extends JPanel {
     private static final Font SEGOE_UI = new Font("Segoe UI", Font.PLAIN, 14);
+    private final JLabel addToBikeLabel = new JLabel("Add to Bike");
 
 
     public ProductPanel(Product product) {
@@ -28,37 +29,36 @@ public class ProductPanel extends JPanel {
         JCheckBox select = new JCheckBox();
         select.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JLabel addToBikeLabel = new JLabel("Add to Bike");
         addToBikeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        GroupLayout layout = new GroupLayout(this);
-        this.setLayout(layout);
+        GroupLayout productPanel5Layout = new GroupLayout(this);
+        this.setLayout(productPanel5Layout);
         JSeparator horizontalSeparator = new JSeparator();
         JSeparator verticalSeparator = new JSeparator(SwingConstants.VERTICAL);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
+        productPanel5Layout.setHorizontalGroup(
+                productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(productPanel5Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addGroup(productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addComponent(horizontalSeparator, GroupLayout.Alignment.TRAILING)
                                         .addComponent(image, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addComponent(horizontalSeparator)
-                                        .addGroup(layout.createSequentialGroup()
+                                        .addGroup(productPanel5Layout.createSequentialGroup()
                                                 .addGap(6, 6, 6)
                                                 .addComponent(displayName, GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE))
-                                        .addGroup(layout.createSequentialGroup()
+                                        .addGroup(productPanel5Layout.createSequentialGroup()
                                                 .addComponent(displayCost, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                                                 .addComponent(verticalSeparator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                                .addGroup(productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                                                         .addComponent(select, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                                         .addComponent(addToBikeLabel, GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE))))
                                 .addContainerGap())
         );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
+        productPanel5Layout.setVerticalGroup(
+                productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(productPanel5Layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addComponent(image, GroupLayout.PREFERRED_SIZE, 185, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
@@ -67,19 +67,23 @@ public class ProductPanel extends JPanel {
                                 .addComponent(displayName)
                                 .addGap(3, 3, 3)
                                 .addComponent(horizontalSeparator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(productPanel5Layout.createSequentialGroup()
                                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                                .addGroup(productPanel5Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                                         .addComponent(verticalSeparator)
                                                         .addComponent(displayCost, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                                 .addContainerGap())
-                                        .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGroup(GroupLayout.Alignment.TRAILING, productPanel5Layout.createSequentialGroup()
                                                 .addGap(12, 12, 12)
                                                 .addComponent(addToBikeLabel)
                                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
                                                 .addComponent(select)
                                                 .addGap(15, 15, 15))))
         );
+    }
+
+    public void setSelectLabel(String label) {
+        this.addToBikeLabel.setText(label);
     }
 }
